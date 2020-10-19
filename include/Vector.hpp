9a50f4d5c38052a33b797760863f189e030688df
l-yohai <yohan9612@naver.com>
2020-10-19 09:55:57 +0900

    update

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: yohlee <yohlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 08:58:20 by yohlee            #+#    #+#             */
-/*   Updated: 2020/10/19 09:14:51 by yohlee           ###   ########.fr       */
+/*   Updated: 2020/10/19 09:38:55 by yohlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,21 +48,27 @@ public:
 	/*                                                                        */
 	/* ====================================================================== */
 
+	/* Default */
 	Vector();
-
-	/* default constructor */
-	explicit Vector(const allocator_type& alloc = allocator_type());
+	explicit Vector(const allocator_type& alloc = allocator_type())
+	: _allocator(alloc), _begin(0), _end(0), _end_capacity(0) {}
+	
+	/* Fill */
 	explicit Vector(size_type n, const value_type& val = value_type(),
-					const allocator_type& alloc = allocator_type());
+					const allocator_type& alloc = allocator_type())
+	: _allocator(alloc), _begin(0), _end(0), _end_capacity(0)
+	{ if (n) assign(n, val); }
 
+	/* Range */
 	template <class InputIterator>
-		Vector(InputIterator first, InputIterator last,
-				const allocator_type& alloc = allocator_type());
+	Vector(InputIterator first, InputIterator last,
+			const allocator_type& alloc = allocator_type())
+	: _allocator(alloc), _begin(0), _end(0), _end_capacity(0)
+	{ assign(first, last); }
 
-	Vector(const Vector& x);
-
-	/* Overload */
-	Vector& operator=(const Vector& );
+	/* Copy */
+	Vector(const Vector& x)
+	: _allocator(x._allocator), _begin(0), _end(0), _end_capacity(0) { operator=(x); }
 
 	/* ====================================================================== */
 	/*                  ___                 _                 _               */
@@ -73,8 +79,13 @@ public:
 	/*                                                                        */
 	/* ====================================================================== */
 
-	/* Destructor */
-	virtual ~Vector();
+	Vector& operator=(const Vector& x)
+	{
+		if (this != &x)
+			assign(x.begin(), x.end());
+		return (*this);
+	}
+
 	/* ====================================================================== */
 	/*               ___          _                   _                       */
 	/*              /   \___  ___| |_ _ __ _   _  ___| |_ ___  _ __           */
@@ -83,6 +94,11 @@ public:
 	/*           /___,' \___||___/\__|_|   \__,_|\___|\__\___/|_|             */
 	/*                                                                        */
 	/* ====================================================================== */
+
+	virtual ~Vector()
+	{
+		deallocate();
+	}
 
 	/* ====================================================================== */
 	/*                          ___     _   _                                 */
